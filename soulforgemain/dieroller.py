24a00260f01978cf95026916be74c003a97ed789
xiphux <tailor@centraldogma>
2005-07-01 03:34:16 -0400

    [project @ 21] Added botch/difficulty gui options

--- soulforgemain/dieroller.py
+++ soulforgemain/dieroller.py
@@ -6,6 +6,9 @@ DIEROLLER_ROLL = 105
 DIEROLLER_POOL = 106
 DIEROLLER_FACES = 107
 DIEROLLER_RNG = 108
+DIEROLLER_BOTCH = 109
+DIEROLLER_TAB = 110
+DIEROLLER_DIFF = 111
 
 class dieroller(wxFrame):
     def __init__(self,parent,ID,title):
@@ -18,13 +21,13 @@ class dieroller(wxFrame):
 	controls = wxBoxSizer(wxVERTICAL)
 
 	poolbox = wxBoxSizer(wxHORIZONTAL)
-	poolbox.Add(wxStaticText(self,-1,u"Dice pool:"),0)
+	poolbox.Add(wxStaticText(self,-1,u"Dice pool:"),0,wxALIGN_CENTER_VERTICAL)
 	self.poolctl = wxSpinCtrl(self,DIEROLLER_POOL,u"",wxDefaultPosition,wxDefaultSize,0,1,99,self.dicepool.pool)
 	poolbox.Add(self.poolctl,1)
 	controls.Add(poolbox,1,wxEXPAND)
 
 	facebox = wxBoxSizer(wxHORIZONTAL)
-	facebox.Add(wxStaticText(self,-1,u"Die faces:"),0)
+	facebox.Add(wxStaticText(self,-1,u"Die faces:"),0,wxALIGN_CENTER_VERTICAL)
 	self.facectl = wxSpinCtrl(self,DIEROLLER_FACES,u"",wxDefaultPosition,wxDefaultSize,0,2,99,self.dicepool.faces)
 	facebox.Add(self.facectl,1)
 	controls.Add(facebox,1,wxEXPAND)
@@ -35,6 +38,23 @@ class dieroller(wxFrame):
 	self.rngctl = wxChoice(self,DIEROLLER_RNG,wxDefaultPosition,wxDefaultSize,rngstrings)
 	rngbox.Add(self.rngctl,1,wxEXPAND)
 	controls.Add(rngbox,0,wxEXPAND)
+	
+	self.botchctl = wxCheckBox(self,DIEROLLER_BOTCH,u"Botches")
+	self.botchctl.SetValue(self.dicepool.botch)
+	controls.Add(self.botchctl,0,wxEXPAND)
+
+	self.tabctl = wxCheckBox(self,DIEROLLER_TAB,u"Tabulate")
+	self.tabctl.SetValue(self.dicepool.tabulate)
+	controls.Add(self.tabctl,0,wxEXPAND)
+
+	diffbox = wxBoxSizer(wxHORIZONTAL)
+	self.difflabel = wxStaticText(self,-1,u"Difficulty:")
+	diffbox.Add(self.difflabel,0,wxALIGN_CENTER_VERTICAL)
+	self.diffctl = wxSpinCtrl(self,DIEROLLER_DIFF,u"",wxDefaultPosition,wxDefaultSize,0,1,self.dicepool.faces,6)
+	diffbox.Add(self.diffctl,1)
+	controls.Add(diffbox,1,wxEXPAND)
+	self.difflabel.Enable(self.dicepool.tabulate)
+	self.diffctl.Enable(self.dicepool.tabulate)
 
 	roll = wxButton(self,DIEROLLER_ROLL,u"Roll")
 	controls.Add(roll,0,wxEXPAND)
@@ -58,6 +78,9 @@ class dieroller(wxFrame):
 	EVT_SPINCTRL(self,DIEROLLER_POOL,self.onpool)
 	EVT_SPINCTRL(self,DIEROLLER_FACES,self.onfaces)
 	EVT_CHOICE(self,DIEROLLER_RNG,self.onrng)
+	EVT_SPINCTRL(self,DIEROLLER_DIFF,self.ondiff)
+	EVT_CHECKBOX(self,DIEROLLER_BOTCH,self.onbotch)
+	EVT_CHECKBOX(self,DIEROLLER_TAB,self.ontab)
 
     def onclose(self,event):
         self.Destroy()
@@ -77,3 +100,14 @@ class dieroller(wxFrame):
 
     def onrng(self,event):
         self.dicepool.setrng(self.rngctl.GetStringSelection())
+
+    def ondiff(self,event):
+        self.dicepool.difficulty = self.diffctl.GetValue()
+
+    def onbotch(self,event):
+        self.dicepool.botch = self.botchctl.GetValue()
+
+    def ontab(self,event):
+        self.dicepool.tabulate = self.tabctl.GetValue()
+	self.difflabel.Enable(self.dicepool.tabulate)
+	self.diffctl.Enable(self.dicepool.tabulate)
