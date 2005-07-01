ca3fd79bdbf29bfaf5cca2299a85d1356180cd92
xiphux <tailor@centraldogma>
2005-07-01 11:39:48 -0400

    [project @ 24] GPL Notice and centered windows

--- libsoulforge/dicepool.py
+++ libsoulforge/dicepool.py
@@ -1,3 +1,24 @@
+#
+# Soulforge
+# Copyright (C) 2005 Christopher Han <xiphux@gmail.com>
+#
+# This file is part of Soulforge.
+#
+# Soulforge is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Soulforge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Soulforge; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
 import random
 
 class dicepool(object):
