0db93f52b7e86b69a912cd656fb90296c2acc0b6
xiphux <tailor@centraldogma>
2005-07-12 13:58:05 -0400

    [project @ 64] Get rid of ugly-ass dark grey default color on MSW part 3

--- soulforgemain/sfrootframe.py
+++ soulforgemain/sfrootframe.py
@@ -80,7 +80,7 @@ class sfrootframe(wxFrame):
 	
 	self.SetMenuBar(menubar)
 
-	pan = wxPanel(self,-1,wxDefaultPosition,wxDefaultSize)
+	pan = wxPanel(self,-1,wxDefaultPosition,self.GetSize())
 
 	root = wxFlexGridSizer(6,2,0,0)
 	root.AddGrowableCol(1,1)
