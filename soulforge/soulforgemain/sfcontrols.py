a428370eea0918273a1ab1721ac3645334ec3336
xiphux <tailor@centraldogma>
2005-07-13 08:42:37 -0400

    [project @ 85] Start implementing homebrew sfpool box

--- soulforge/soulforgemain/sfcontrols.py
+++ soulforge/soulforgemain/sfcontrols.py
@@ -38,7 +38,6 @@ class sfdot(buttons.GenToggleButton):
 
         self.up = True
         self.hasFocus = False
-        self.bezelWidth = 0
         self.useFocusInd = False
 
         self.InheritAttributes()
@@ -57,11 +56,11 @@ class sfdot(buttons.GenToggleButton):
         self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
         self.Bind(wx.EVT_PAINT,            self.OnPaint)
 
-    def OnPoint(self, event):
+    def OnPaint(self, event):
         (width, height) = self.GetClientSizeTuple()
 	x = width - 1
 	y = height - 1
-	df = wx.BufferedPaintDC(self)
+	dc = wx.BufferedPaintDC(self)
 	dc.Clear()
 	self.DrawBezel(dc, 0, 0, x, y)
 
@@ -71,7 +70,73 @@ class sfdot(buttons.GenToggleButton):
 	    dc.SetBrush(wx.Brush(self.GetBackgroundColour(), wx.SOLID))
 	else:
 	    dc.SetBrush(wx.Brush(wx.BLACK, wx.SOLID))
-	dc.DrawEllipseRect(wx.Rect(self.bezelWidth+2,self.bezelWidth+2,y2-self.bezelWidth*2-4,y2-self.bezelWidth*2-4))
+	dc.DrawEllipseRect(wx.Rect(2,2,y2-4,y2-4))
+
+    def DoGetBestSize(self):
+        width = 15
+	height = 15
+	return (width, height)
+
+    def DrawFocusIndicator(self, dc, w, h):
+        pass
+
+    def OnLeftDown(self, event):
+        if not self.IsEnabled():
+            return
+        self.saveUp = self.up
+        self.up = not self.up
+        self.CaptureMouse()
+        self.SetFocus()
+
+
+class sfbox(buttons.GenToggleButton):
+    def __init__(self, parent, ID, label,
+                 pos = wx.DefaultPosition, size = wx.DefaultSize,
+                 style = 0, validator = wx.DefaultValidator,
+                 name = "genbutton"):
+        cstyle = style
+        if cstyle == 0:
+            cstyle = wx.NO_BORDER
+        wx.PyControl.__init__(self, parent, ID, pos, size, cstyle, validator, name)
+
+        self.up = True
+        self.hasFocus = False
+        self.useFocusInd = False
+
+        self.InheritAttributes()
+        self.SetBestFittingSize(size)
+        self.InitColours()
+
+        self.Bind(wx.EVT_LEFT_DOWN,        self.OnLeftDown)
+        self.Bind(wx.EVT_LEFT_UP,          self.OnLeftUp)
+        if wx.Platform == '__WXMSW__':
+            self.Bind(wx.EVT_LEFT_DCLICK,  self.OnLeftDown)
+        self.Bind(wx.EVT_MOTION,           self.OnMotion)
+        self.Bind(wx.EVT_SET_FOCUS,        self.OnGainFocus)
+        self.Bind(wx.EVT_KILL_FOCUS,       self.OnLoseFocus)
+        self.Bind(wx.EVT_KEY_DOWN,         self.OnKeyDown)
+        self.Bind(wx.EVT_KEY_UP,           self.OnKeyUp)
+        self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
+        self.Bind(wx.EVT_PAINT,            self.OnPaint)
+
+    def OnPaint(self, event):
+        (width, height) = self.GetClientSizeTuple()
+	x = width - 1
+	y = height - 1
+	dc = wx.BufferedPaintDC(self)
+	dc.Clear()
+	self.DrawBezel(dc, 0, 0, x, y)
+	self.DrawMarker(dc, 0, 0, x, y)
+
+    def DrawMarker(self, dc, x1, y1, x2, y2):
+        dc.SetPen(wx.BLACK_PEN)
+	if not self.up:
+	    dc.DrawLine(x2-3, 2, 2, y2-3)
+
+    def DrawBezel(self, dc, x1, y1, x2, y2):
+	dc.SetPen(wx.BLACK_PEN)
+	dc.SetBrush(wx.Brush(self.GetBackgroundColour(), wx.SOLID))
+	dc.DrawRectangle(2,2,y2-4,y2-4)
 
     def DoGetBestSize(self):
         width = 15
@@ -108,10 +173,10 @@ class sfstat(wx.Panel):
 
 	for i in range(self.btns):
 	    if alternate:
-	        pass
+	        self.buttons.append(sfbox(self,(SFSTAT_BUTTON + i),""))
 	    else:
 	        self.buttons.append(sfdot(self,(SFSTAT_BUTTON + i),""))
-		wx.EVT_BUTTON(self,(SFSTAT_BUTTON+i),self.onclick)
+            wx.EVT_BUTTON(self,(SFSTAT_BUTTON+i),self.onclick)
 	    root.Add(self.buttons[i],1,wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
 
 	self.recalc()
@@ -149,30 +214,19 @@ class sfpool(wx.Panel):
 	root = wx.GridSizer(rows,cols,1,1)
 
 	self.buttons = []
-	self.dummy = []
-	self.state = []
 
 	for i in range(self.total):
 	    if alternate:
-	        self.buttons.append(wx.RadioButton(self,(SFPOOL_BUTTON + i),"",wx.DefaultPosition,wx.DefaultSize,wx.RB_GROUP))
-		self.dummy.append(wx.RadioButton(self,-1,"",wx.DefaultPosition,wx.DefaultSize,wx.RB_SINGLE))
-		self.dummy[i].Show(False)
-		self.dummy[i].SetValue(True)
-		self.state.append(False)
-		wx.EVT_RADIOBUTTON(self,(SFPOOL_BUTTON + i),self.onclick)
+	        self.buttons.append(sfdot(self,(SFPOOL_BUTTON + i),""))
 	    else:
-	        self.buttons.append(wx.CheckBox(self,(SFPOOL_BUTTON + i),""))
-		wx.EVT_CHECKBOX(self,(SFPOOL_BUTTON + i),self.onclick)
+	        self.buttons.append(sfbox(self,(SFPOOL_BUTTON + i),""))
+	    wx.EVT_BUTTON(self,(SFPOOL_BUTTON + i),self.onclick)
 	    root.Add(self.buttons[i],0,wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
 	
 	self.SetSizer(root)
 
     def onclick(self,event):
         n = event.GetId() - SFPOOL_BUTTON
-        if self.alternate:
-	    if self.state[n]:
-	        self.dummy[n].SetValue(True)
-	    self.state[n] = self.buttons[n].GetValue()
         self.recalc()
 
     def recalc(self):
@@ -180,8 +234,6 @@ class sfpool(wx.Panel):
 	for i in range(self.total):
 	    if self.buttons[i].GetValue():
 	        n += 1
-            if self.alternate:
-	        self.state[i] = self.buttons[i].GetValue()
         self.value = n
 
     def setvalue(self,v):
@@ -190,5 +242,3 @@ class sfpool(wx.Panel):
 	    self.buttons[i].SetValue(True)
 	for i in range(self.value,len(self.buttons)):
 	    self.buttons[i].SetValue(False)
-	    if self.alternate:
-	        self.dummy[i].SetValue(True)
